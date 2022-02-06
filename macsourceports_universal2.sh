725fec7865b289298b4b29b338279fefec679b09
Tom Kidd <macsourceports@gmail.com>
2022-02-05 18:51:59 -0600

    Refinements to build process

--- macsourceports_universal2.sh
+++ macsourceports_universal2.sh
@@ -7,37 +7,30 @@ export EXECUTABLE_NAME="dhewm3"
 export PKGINFO="APPLDHM3"
 export COPYRIGHT_TEXT="DOOM 3 Copyright © 2004 id Software, Inc. All rights reserved."
 
-# constants
-export BUILT_PRODUCTS_DIR="release"
-export WRAPPER_NAME="${PRODUCT_NAME}.app"
-export CONTENTS_FOLDER_PATH="${WRAPPER_NAME}/Contents"
-export EXECUTABLE_FOLDER_PATH="${CONTENTS_FOLDER_PATH}/MacOS"
-export UNLOCALIZED_RESOURCES_FOLDER_PATH="${CONTENTS_FOLDER_PATH}/Resources"
-export ICONS="${ICONSFILENAME}.icns"
-export BUNDLE_ID="com.macsourceports.${PRODUCT_NAME}"
+#constants
+source ../MSPScripts/constants.sh
 
-# For parallel make on multicore boxes...
-NCPU=`sysctl -n hw.ncpu`
+rm -rf ${BUILT_PRODUCTS_DIR}
 
 # create makefiles with cmake
-rm -rf build-x86_64
-mkdir build-x86_64
-cd build-x86_64
+rm -rf ${X86_64_BUILD_FOLDER}
+mkdir ${X86_64_BUILD_FOLDER}
+cd ${X86_64_BUILD_FOLDER}
 /usr/local/bin/cmake -G "Unix Makefiles" -DCMAKE_C_FLAGS_RELEASE="-arch x86_64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DSDL2=ON -DOPENAL_LIBRARY=~/Documents/GitHub/MSPStore/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=~/Documents/GitHub/MSPStore/opt/openal-soft/include -DSDL2_INCLUDE_DIRS=/usr/local/opt/sdl2/include/SDL2 -DSDL2_LIBRARIES=/usr/local/opt/sdl2/lib ../neo -Wno-dev
 
 cd ..
-rm -rf build-arm64
-mkdir build-arm64
-cd build-arm64
+rm -rf ${ARM64_BUILD_FOLDER}
+mkdir ${ARM64_BUILD_FOLDER}
+cd ${ARM64_BUILD_FOLDER}
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DSDL2=ON -DOPENAL_LIBRARY=~/Documents/GitHub/MSPStore/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=~/Documents/GitHub/MSPStore/opt/openal-soft/include ../neo -Wno-dev
 
 # perform builds with make
 cd ..
-cd build-x86_64
+cd ${X86_64_BUILD_FOLDER}
 make -j$NCPU
 
 cd ..
-cd build-arm64
+cd ${ARM64_BUILD_FOLDER}
 make -j$NCPU
 
 cd ..
@@ -50,22 +43,8 @@ if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/base" ]; then
 	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/base" || exit 1;
 fi
 
-#lipo the executable
-lipo build-x86_64/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME} build-arm64/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME} -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}" -create
-lipo build-x86_64/base.dylib build-arm64/base.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/base/base.dylib" -create
-
-cp ~/Documents/GitHub/MSPStore/lib/libSDL2-2.0.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-cp ~/Documents/GitHub/MSPStore/Cellar/openal-soft/1.21.1/lib/libopenal.1.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-
-# use install_name tool to point executable to bundled resources (probably wrong long term way to do it)
-#modify x86_64
-install_name_tool -change /usr/local/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/libSDL2-2.0.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /usr/local/opt/openal-soft/lib/libopenal.1.dylib @executable_path/libopenal.1.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-#modify arm64
-install_name_tool -change /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/libSDL2-2.0.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /opt/homebrew/opt/openal-soft/lib/libopenal.1.dylib @executable_path/libopenal.1.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-
-echo "bundle done."
+#lipo any app-specific things
+lipo ${X86_64_BUILD_FOLDER}/base.dylib ${ARM64_BUILD_FOLDER}/base.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/base/base.dylib" -create
 
 #sign and notarize
 "../MSPScripts/sign_and_notarize.sh" "$1"
